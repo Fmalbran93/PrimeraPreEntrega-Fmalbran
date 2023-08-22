797ab2c733399bbbed3fa0bcd55e2ac7b0cdc3f6
fmalbran93 <fmalbran93@gmail.com>
2023-08-22 02:15:24 -0300

    Agrego botones para ir directamente a las rutas /static y /static/realtimeproducts

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,24 +1,33 @@
 const socket = io();
 
 const productsContainer = document.querySelector('#products-container');
+const goToRealTimeProds = document.querySelector('#goToRealTimeProds');
 
 socket.emit('load');
 
-socket.on('products', products => {
-	productsContainer.innerHTML = '';
-	products.forEach(prod => {
-		productsContainer.innerHTML += `
-    <div class="product-container">
-      <p>Id: ${prod.id}</p>
-      <p>Title: ${prod.title}</p>
-      <p>Description: ${prod.description}</p>
-      <p>Price: ${prod.price}</p>
-      <p>Status: ${prod.status}</p>
-      <p>Code: ${prod.code}</p>
-      <p>Stock: ${prod.stock}</p>
 
-    </div>
-  
-    `;
-	});
+socket.on('products', products => {
+	if (products.length === 0) {
+		productsContainer.innerHTML = '<h3> No hay productos agregados al momento, ve a <a href="/static/realtimeproducts">/static/realtimeproducts</a> para agregarlos!.</h3>';
+	} else {
+		productsContainer.innerHTML = '';
+		products.forEach(prod => {
+			productsContainer.innerHTML += `
+			<div class="product-container">
+				<p>Id: ${prod.id}</p>
+				<p>Title: ${prod.title}</p>
+				<p>Description: ${prod.description}</p>
+				<p>Price: ${prod.price}</p>
+				<p>Status: ${prod.status}</p>
+				<p>Code: ${prod.code}</p>
+				<p>Stock: ${prod.stock}</p>
+			</div>
+			`;
+		});
+	}
 });
+
+	// Agregue el evento al botón de redirección a realtimeprods
+  goToRealTimeProds.addEventListener('click', () => {
+    window.location.href = '/static/realtimeproducts';
+  });
--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -1,40 +1,48 @@
-const socket = io();
+	const socket = io();
 
-const form = document.querySelector('#formProduct');
-const productsContainer = document.querySelector('#products-container');
+	const form = document.querySelector('#formProduct');
+	const productsContainer = document.querySelector('#products-container');
+	const goToStaticButton = document.querySelector('#goToStaticButton');
+	socket.emit('load');
 
-socket.emit('load');
-
-form.addEventListener('submit', event => {
-	event.preventDefault();
-	const dataForm = new FormData(event.target);
-	const product = Object.fromEntries(dataForm);
-	Swal.fire({
-		title: '¡Producto creado!',
-		text: 'El producto ha sido creado exitosamente.',
-		icon: 'success',
-		showCancelButton: false,
-		confirmButtonColor: 'rgb(184, 125, 192)',
-		confirmButtonText: 'Aceptar'
-	  });
-	socket.emit('newProduct', product);
-});
-
-socket.on('products', products => {
-	productsContainer.innerHTML = '';
-	products.forEach(prod => {
-		productsContainer.innerHTML += `
-    <div class="product-container">
-      <p>Id: ${prod.id}</p>
-      <p>Title: ${prod.title}</p>
-      <p>Description: ${prod.description}</p>
-      <p>Price: ${prod.price}</p>
-      <p>Status: ${prod.status}</p>
-      <p>Code: ${prod.code}</p>
-      <p>Stock: ${prod.stock}</p>
-
-    </div>
-  
-    `;
+	form.addEventListener('submit', event => {
+		event.preventDefault();
+		const dataForm = new FormData(event.target);
+		const product = Object.fromEntries(dataForm);
+		Swal.fire({
+			title: '¡Producto creado!',
+			text: 'El producto ha sido creado exitosamente.',
+			icon: 'success',
+			showCancelButton: false,
+			confirmButtonColor: 'rgb(184, 125, 192)',
+			confirmButtonText: 'Aceptar'
+		});
+		socket.emit('newProduct', product);
 	});
+
+
+	socket.on('products', products => {
+		productsContainer.innerHTML = '';
+		products.forEach(prod => {
+			productsContainer.innerHTML += `
+		<div class="product-container">
+		<p>Id: ${prod.id}</p>
+		<p>Title: ${prod.title}</p>
+		<p>Description: ${prod.description}</p>
+		<p>Price: ${prod.price}</p>
+		<p>Status: ${prod.status}</p>
+		<p>Code: ${prod.code}</p>
+		<p>Stock: ${prod.stock}</p>
+
+		</div>
+	
+		`;
+		});
+
+		e.target.reset();
+	});
+
+	// Agregar el evento al botón de redirección
+goToStaticButton.addEventListener('click', () => {
+	window.location.href = '/static';
 });
